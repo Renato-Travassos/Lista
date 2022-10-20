d4f50fbca2a29c5fc8f87741924ff122e1051a93
Renato Travassos <95237610+Renato-Travassos@users.noreply.github.com>
2022-10-20 10:53:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ def programalista(n):
    for c in range(n):
       r=input('nome:').upper().strip()
       lista.append(r)
+      
+def listas(nome):
+     print('para sair digite 999\n') 
+     for c,v in enumerate(lista):       
+        print(f'{c} {v}')
+     print('-'*20)
+      
 while select!='5': 
  select=input('SELECIONE \n0 =apaga,1=adiciona,2=pesquisa,3=ver lista,4=editar,5=terminar')
  if select=='0':  
     print('-'*20)
     while nome!='999':    
-     for c,v in enumerate(lista):       
-        print(f'{c} {v}')
-     print('-'*20)
-     nome=input('digite o número do nome') 
+     listas(nome)
+     nome=input('digite o número do nome: ') 
      if nome.isnumeric() and nome!='999':
         nome=int(nome)
         if nome<=len(lista)-1:
@@ -28,15 +33,13 @@ while select!='5':
         programalista(n)          
         break    
  elif select=='2':
-    print('para sair digite 999\n')
     search=' '
     while search!='999':
      search=input('ver se existe tal nome: ').upper().strip()
      print(f'{search} está na lista?',search in lista)
  elif select=='4':
       while nome!='999':
-       for c,v in enumerate(lista):
-           print(c,v)     
+       listas(nome)        
        nome=input('digite o nome que quer colocar').upper().strip()
        edit=input('numero')
        if len(nome)>3 and edit.isnumeric():
@@ -46,8 +49,8 @@ while select!='5':
              print('editado')
        else:
           print('número inexistente')       
- elif select=='3' or select=='5':
+ elif select in '35' :
       print('-'*20)
       for nome in lista:
-         print(nome,end='\n') 
+         print(nome) 
       print('-'*20)     
